babed4e9b643b5b4da65dd609487bff8ae9ccde0
Andre Cadima <andrecadimaschool@gmail.com>
2025-10-15 23:44:53 -0400

    Rest of Repositories

---
+++ SistemaDePagoDeAranceles/Domain/Models/Fee.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaDePagoDeAranceles.Domain.Models
+{
+    public class Fee
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int Year { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Description { get; set; }
+        public Category? Category { get; set; }
+    }
+}
