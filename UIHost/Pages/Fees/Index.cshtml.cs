92d276f91fa9b8b922047734ba794a0fde086ba9
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2025-10-30 00:42:36 -0400

    add fees index page and repository di in program cs

---
+++ UIHost/Pages/Fees/Index.cshtml.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using UIHost.Security;
+using Common.Domain.Patterns;
+using Common.Domain.SharedPorts;
+using Microsoft.AspNetCore.Mvc;
+using TariffingService.Domain.Models;
+
+namespace UIHost.Pages.Fees
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IRepositoryService<Fee> _repository;
+        private readonly IRepositoryService<Category> _categoryRepository;
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
+        public Result<IEnumerable<Fee>> ResultFeeGetAll { get; set; }
+
+        public List<Fee> Fees { get; set; } = new();
+
+        public IndexModel(IRepositoryServiceFactory<Fee> factory, IRepositoryServiceFactory<Category> categoryFactory, IdProtector idProtector)
+        {
+            _repository = factory.CreateRepositoryService();
+            _categoryRepository = categoryFactory.CreateRepositoryService();
+        }
+
+        public void OnGet()
+        {
+            ResultFeeGetAll = _repository.GetAll();
+
+            foreach (var fee in ResultFeeGetAll.Value)
+            {
+                fee.Category = _categoryRepository.GetAll().Value.FirstOrDefault(c => c.Id == fee.Id);
+            }
+        }
+
+        public void OnPostSearch()
+        {
+            ResultFeeGetAll = string.IsNullOrWhiteSpace(SearchTerm) ? _repository.GetAll() : _repository.Search(SearchTerm);
+            Fees = ResultFeeGetAll.Value?.ToList() ?? new List<Fee>();
+        }
+    }
+}
--- UIHost/Program.cs
+++ UIHost/Program.cs
@@ -49,6 +49,16 @@ builder.Services.AddScoped<IRepositoryServiceFactory<PersonInCharge>, PersonInCh
 builder.Services.AddSingleton<IDbRepository<Establishment>, EstablishmentRepository>();
 builder.Services.AddScoped<IRepositoryServiceFactory<Establishment>, EstablishmentRepositoryServiceCreator>();
 
+// ==========================
+//  FEE CONFIG
+// ==========================
+
+builder.Services.AddSingleton<IDbRepository<Fee>, FeeRepository>();
+builder.Services.AddScoped<IRepositoryServiceFactory<Fee>, FeeRepositoryServiceCreator>();
+
+// ==========================
+//  USER CONFIG
+// ==========================
 
 builder.Services.AddSingleton<IDbRepository<User>, UserRepository>();
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
