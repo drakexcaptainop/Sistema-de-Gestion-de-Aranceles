2986f29ff81a5764f7bb97f202893588044ccd04
cazadorDeColombianos <90215784+drakexcaptainop@users.noreply.github.com>
2025-10-30 12:45:06 -0400

    Fix report 1

--- UIHost/Program.cs
+++ UIHost/Program.cs
@@ -17,6 +17,7 @@ using UserManagementService.Domain.Models;
 using UserManagementService.Domain.Ports;
 using UserManagementService.Infrastructure.Adapters;
 using ReportService.Application;
+using ReportService.Infrastructure.RepositoryAdapters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -61,7 +62,8 @@ builder.Services.AddScoped<IRepositoryServiceFactory<User>, UserRepositoryServic
 builder.Services.AddScoped<IUserRepositoryService, UserRepositoryService>();
 
 // 🔹 Report service registration
-
+builder.Services.AddScoped<EstablishmentReportService>();
+builder.Services.AddScoped<EstablishmentWithPersonDtoRepository>();
 
 
 var _configuration = builder.Configuration;
