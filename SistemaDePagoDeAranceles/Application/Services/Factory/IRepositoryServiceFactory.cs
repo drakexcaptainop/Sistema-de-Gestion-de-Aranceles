7aef20a5744bf6e7009b392b41eb437e6e356472
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2025-11-04 15:20:49 -0400

    merge main to presentacion branch

--- SistemaDePagoDeAranceles/Application/Services/Factory/IRepositoryServiceFactory.cs
+++
@@ -1,8 +0,0 @@
-using SistemaDePagoDeAranceles.Application.Services.RepositoryServices;
-
-namespace SistemaDePagoDeAranceles.Application.Services.Factory;
-
-public interface IRepositoryServiceFactory<T>
-{
-    public IRepositoryService<T> CreateRepositoryService();
-}
